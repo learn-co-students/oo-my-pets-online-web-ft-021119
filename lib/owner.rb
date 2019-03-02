ca5689d322ff3d70bbea328f222b19c9e7d8ec93
TaylorSwinton <taylorswinton1@gmail.com>
2019-03-01 20:00:58 -0600

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
-  # code goes here
+  
+  @@all = []
+  @@count = 0
+  def initialize(species)
+    @species = species
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
+    @@count += 1
+  end
+
+  def species
+    @species
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
 end
