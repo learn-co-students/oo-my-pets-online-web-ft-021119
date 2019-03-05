d61095a03fcf86750474aa473c634cc002036b64
Burak Kocak <burakkocak884@ghotmail.com>
2019-03-05 10:16:38 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
+
+
 class Dog
+
+   attr_accessor :mood
+   attr_reader :name
+
+
+   def initialize(name , mood = "nervous")
+   	@name = name
+   	@mood = mood
+   end
+
+
   # code goes here
 end
