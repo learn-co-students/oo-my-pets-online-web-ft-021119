43d947c64d8b1530c683cf5bc54bbe84190d1070
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-05 17:21:27 +0000

    Done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,5 +1,11 @@
 class Fish
+  attr_accessor :mood
   def initialize(name)
     @name = name
+    @mood = "nervous"
+  end
+
+  def name
+    @name
   end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,44 @@
+require 'pry'
 class Owner
-  # code goes here
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @pets = {}
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def species
+    @species
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def pets
+    @pets[:fishes] = []
+    @pets[:dogs] = []
+    @pets[:cats] = []
+    @pets
+  end
+
+  def buy_fish(fish)
+    @pets[:fishes] = fish
+    @pets
+    # binding.pry
+  end
 end
